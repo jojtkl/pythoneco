cb75c34cf833f25cb4c2333f2625df91f84b2273
jojtkl <jojtkl@gmail.com>
2023-02-27 12:21:08 +0530

    added

---
+++ ecommerceproject/shopapp/urls.py
@@ -0,0 +1,14 @@
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.urls import path, include
+
+app_name = 'shopapp'
+urlpatterns = [
+
+    path('', views.allProdCat, name='allProdCat'),
+    path('<slug:c_slug>/', views.allProdCat, name='products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/', views.proDetail, name='prodCatdetail')
+
+]
